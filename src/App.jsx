c86baffecb68d1761d36ba507971041ffa35b16a
Advik <advik2040@gmail.com>
2026-02-20 17:28:04 +0530

    Initial clean commit for HRMS project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>HRMS Project Started</h1>
+    </div>
+  );
+}
+
+export default App;
